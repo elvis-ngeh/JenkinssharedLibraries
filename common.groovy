02dea252b4f5ece4a74b42e647295de71523b2c0
elvis-ngeh <104249773+elvis-ngeh@users.noreply.github.com>
2023-01-06 01:24:38 -0500

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,14 +1,14 @@
 def call (String stageName){
     
- if('"${stageName}" == "Build"')
+ if("${stageName}" == "Build")
 }
 sh "mvn clean package"
 {
- else if ('"${stageName}" == "SonarqubeReport"')
+ else if ("${stageName}" == "SonarqubeReport")
 }
 sh " mvn sonar:sonar"
 {
- else if ('"${stageName}" == "Upload to Nexus"')
+ else if ("${stageName}" == "Upload to Nexus")
 }
 sh "mvn deploy"
 {
