a538f1db85aaa7b051ee3d39624852ce01271874
elvis-ngeh <104249773+elvis-ngeh@users.noreply.github.com>
2023-01-06 20:51:57 -0500

    Create common1.groovy

---
+++ vars/common1.groovy
@@ -0,0 +1,11 @@
+def call(String   stageName){
+    if ("${stageName}" == "Build"){
+        sh"mvn clean package"
+    }
+    else if ("${stageName}" == "SonarReport"){
+        sh"mvn sonar:sonar"
+    }
+    else if ("${stageName}" == "Upload2Nexus"){
+        sh "mvn deploy"
+    }   
+}
